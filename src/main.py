d88342a4995f3129f3fcaaeb28a546184ba8c3e3
sckintas <sckintas@gmail.com>
2024-08-09 18:56:57 +0200

    fourth commit

--- src/main.py
+++ src/main.py
@@ -6,6 +6,6 @@ router = APIRouter()
 class ResponseBody(BaseModel):
     message: str
 
-@router.get("/hello-Seckin!")
+@router.get("/hello-Seckin Kintas!")
 def prompt() -> ResponseBody:
     return {"message": "Hello, world!"}
